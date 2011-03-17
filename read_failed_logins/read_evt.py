f05fab02ae35eac7fa168d0d8992d29c328f70b1
Alex Volkov <avolkov@gmail.com>
2011-03-17 16:01:55 -0400

    Refractored read_env.py a bit to make it more generic.

--- read_failed_logins/read_evt.py
+++ read_failed_logins/read_evt.py
@@ -4,19 +4,36 @@ import win32evtlog
 import win32evtlogutil
 import string, pickle, sys, os.path
 
-
-def read_faild_login_events():
-	event_log = win32evtlog.OpenEventLog(None, "Security")
+def read_login_events(eventid, log_source):
+	event_log = win32evtlog.OpenEventLog(None, log_source)
 	flags = win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
 	login_attempt_events = []
 	while(True):
 		records = win32evtlog.ReadEventLog(event_log, flags,0)
 		for rec in records:
-			if rec.EventID == 529:
+			if rec.EventID == eventid:
 				login_attempt_events.append(rec)
 		if len(records) == 0:
 			break
 	return login_attempt_events
+
+def read_faild_login_events():
+	def filter_interesting(input):
+		"""
+		@input: list
+		@output: returns list containing [username, login type, src ip, src port]
+		"""
+		return [input[0], int(input[2]), input[12], int(input[13])]
+
+	interesting_data = []
+	for item in read_login_events(529,"Security"):
+		interesting_data.append(format_data(item, filter_interesting))
+	return interesting_data
+
+def format_data(event_log_item, outer_filter):
+	interesting = outer_filter(safe_format_to_list(get_event_string(event_log_item)))
+	interesting.insert(0,event_log_item.TimeGenerated.Format("%Y-%m-%d %H:%M:%S"))
+	return interesting
 
 def get_event_string(event):
 		"""
@@ -31,12 +48,6 @@ def safe_format_to_list(msg):
 			return [string.strip(x, "\"'()<> .,") for x in (msg.split(":")[1]).split(",")]
 		except IndexError:
 			return []
-def filter_interesting(input):
-		"""
-		@input: list
-		@output: returns list containing [username, login type, src ip, src port]
-		"""
-		return [input[0], int(input[2]), input[12], int(input[13])]
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print >> sys.stderr, "Usage: "+sys.argv[0]+" <pickle_save_filename>"
@@ -46,13 +57,8 @@ if __name__ == "__main__":
 		print >> sys.stderr, "Usage: "+sys.argv[0]+" <pickle_save_filename>"
 		sys.exit(2)
 	
-	login_attempts = read_faild_login_events()
 	output = open(sys.argv[1], 'wb')
-	interesting_data = []
-	for login_attempt in login_attempts:
-		interesting = filter_interesting(safe_format_to_list(get_event_string(login_attempt)))
-		interesting.insert(0,login_attempt.TimeGenerated.Format("%Y-%m-%d %H:%M:%S"))
-		interesting_data.append(interesting)
+	interesting_data = read_faild_login_events()
 	#print interesting
 	pickle.dump(interesting_data, output, -1)
 	output.close()
